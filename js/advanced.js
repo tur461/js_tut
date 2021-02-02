79c49861d4342e7c1816642787debfb3796c4ffc
tur461 <tur461.er@gmail.com>
2021-02-02 11:22:27 +0530

    some more code added----call/apply/bind

--- js/advanced.js
+++ js/advanced.js
@@ -43,7 +43,7 @@ function sum(a, b){
 }
 
 function sub(a, b){
-    return a+b;
+    return a-b;
 }
 
 let jstester = {
@@ -56,9 +56,34 @@ let jstester = {
     toBe: function(t){
        if(this.xpt !== t)
             throw new Error(`Test # ${this.testNo} Failed`);
+        console.log(`%cTest # ${this.testNo} Success.`, 'color:lime;')
         ++this.testNo;
     }
+}
 
+function inc(){ // without Closure
+    let someVar = 0;
+    someVar++;
+    return someVar;
+}
+
+function _inc(){ //  with Closure
+    let someVar = 0;
+    return function(){
+        someVar++;
+        return someVar;
+    };
+}
+
+let person1 = {
+    info: function(city, pincode){
+        return this.name + ' is ' + this.age + ', lives in ' + city + ', ' + pincode;
+    }
+}
+
+let person2 = {
+    name: 'wajid',
+    age: 29
 }
 
 function doAdvanced(){
@@ -69,12 +94,57 @@ function doAdvanced(){
     // this in function
     console.log(checkThis());
     
-    // method chaining --- demo of unit testing
+    // method chaining using 'this' --- demo of unit testing
 
     jstester.expect(sum(1,2)).toBe(3)
 
     jstester.expect(sub(1,2)).toBe(-1)
+    
+    
+    // without Closure
+    
+    inc();
+    inc();
+    inc();
+    let i = inc();
+    console.log('i:', i);
+    //jstester.expect(i).toBe(4) // will fail
+    
+    // with closure
+    let myInc = _inc(); // first get closure function from _inc()
+    
+    myInc();
+    myInc();
+    myInc();
+    i = myInc();
+    console.log('i:', i);
+    jstester.expect(i).toBe(4) // will pass
 
+    // usage of bind()
+
+    let c = 'srinagar', p = '190011';
+    person1.info = person1.info.bind(person2)
+    
+    console.log('Info of person1 using person2 -- bind():', person1.info(c, p));
+    
+    // usage of call() -- pass new this temporarily
+    // reset this back to person1
+    person1.info = person1.info.bind(person1)
+    console.log('Info of person1 using person2 -- call():', person1.info.call(person2, c, p));
+    
+    // usage of apply() -- pass new this temporarily
+    console.log('Info of person1 using person2 -- call():', person1.info.apply(person2, [c, p]));
+
+    // call and apply diff in way of param passing.
+
+    // application of call/apply
+    // ex: Math.max()
+
+    console.log('max of 1, 2:', Math.max(1,2));
+    console.log('max of 1, 2, 3:', Math.max(1, 2, 3));
+    // doesnt work on array of numbers but comma separated
+    console.log('max of [1, 2, 3]:', Math.max([1, 2, 3])); // returns NaN
+    console.log('max of [1, 2, 3]:', Math.max.apply(null, [1, 2, 3])); // returns NaN
 
 
 }
